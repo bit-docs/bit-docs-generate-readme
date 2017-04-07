d080f31e72dd480fed537446ec3213bdf5ab152e
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-07 07:54:56 -0500

    prelim docs

--- bit-docs.js
+++ bit-docs.js
@@ -1,5 +1,15 @@
 var generator = require("./bit-docs-generate-readme");
 
+/**
+ * @module {function} bit-docs-generate-readme
+ * @parent plugins
+ *
+ * @description Generates a readme from other docs.
+ *
+ * @body
+ *
+ * TBD
+ */
 module.exports = function(bitDocs){
     bitDocs.register("generator", generator );
 };
